ecb2b880e6d18659716455ca0f70676ba07ff89d
lakshya2530 <112060854+sharmalakshya077@users.noreply.github.com>
2025-07-27 16:13:31 +0530

    updated code

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 
 //const mysql = require('mysql2');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 // const connection = mysql.createConnection({
 //     host: 'localhost',
