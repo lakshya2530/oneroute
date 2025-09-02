3653dccf27f287576e23ba5c58ee38b4a284fd5d
lakshya2530 <112060854+sharmalakshya077@users.noreply.github.com>
2025-09-02 18:30:27 +0530

    updated code

--- config/razorpay.js
+++ config/razorpay.js
@@ -1,9 +1,13 @@
-// config/razorpay.js
 const Razorpay = require("razorpay");
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_SECRET
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+console.log("Razorpay Config:", {
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET ? "loaded" : "missing"
 });
 
 module.exports = razorpay;
