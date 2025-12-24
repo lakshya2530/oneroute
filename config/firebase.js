1427f7d38376cf8cf85fe41cd22bbe6d4fe26450
lakshya2530 <112060854+sharmalakshya077@users.noreply.github.com>
2025-12-24 17:46:40 +0530

    updated code

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,9 @@
 const admin = require("firebase-admin");
-const path = require("path");
 
-const serviceAccount = require(path.join(__dirname, "oneroute-279b4-19c0c45d91b3.json"));
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+  });
+}
 
 module.exports = admin;
